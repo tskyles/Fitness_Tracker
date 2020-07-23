d5915cb8559302eb97d655fcfd2547971e6c595c
travis skyles <travis.skyles@gmail.com>
2020-07-23 13:19:34 -0700

    added icons to Main tabs

--- src/Nav/Main/index.js
+++ src/Nav/Main/index.js
@@ -1,11 +1,10 @@
+import { FontAwesome, MaterialIcons } from '@expo/vector-icons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
-import { StyleSheet } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import Log from '../../Pages/Log';
 import Settings from '../../Pages/Settings';
-
 
 const Tab = createBottomTabNavigator();
 
@@ -14,27 +13,34 @@ export default function Main(props){
   return (
     <SafeAreaProvider>
       <NavigationContainer>
-        <Tab.Navigator>
+        <Tab.Navigator
+          tabBarOptions={{
+            keyboardHidesTabBar: true,
+            showIcon: true,
+          }}
+        >
           <Tab.Screen 
             name='Log'
             component={Log}
+            options={{
+              tabBarLabel: 'Log',
+              tabBarIcon: () => (
+                <FontAwesome name="book" size={24} color="black" />
+              )
+            }}
           />
           <Tab.Screen
             name='Settings'
             component={Settings}
+            options={{
+              tabBarLabel: 'Settings',
+              tabBarIcon: () => (
+                <MaterialIcons name="settings" size={24} color="black" />
+              )
+            }}
           />
         </Tab.Navigator>
       </NavigationContainer>
     </SafeAreaProvider>
   )
-  
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
